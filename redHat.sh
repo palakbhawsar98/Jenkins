6aa534d09cf62d53d9fc2e79d22d4292a32a21aa
Palak Bhawsar <palakbhawsar5@gmail.com>
2022-09-07 14:32:11 +0530

    user data script

---
+++ redHat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install openjdk-8-jdk -y
+sudo yum install maven -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+sudo yum install jenkins -y
+sudo systemctl daemon-reload
